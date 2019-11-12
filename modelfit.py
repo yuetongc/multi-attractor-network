a9e3b87e58cbd321b5417632e44c510120d76440
yuetongc <yuetongyc@outlook.com>
2019-11-12 18:57:27 +0000

    functions to help normalise firing rate in fitting

--- modelfit.py
+++ modelfit.py
@@ -13,3 +13,15 @@ def f_v(x, params):
 
 def mse_fv(params, xdata, ydata):
     return np.sum(np.power(ydata - f_v(xdata, params), 2.))
+
+
+def integral_app(ydata):
+    return np.sum(ydata)
+
+
+def firing_rate(V_in, gain=0.1, max_rate=100):
+    return max_rate * np.tanh(gain * np.maximum(V_in, 0))
+
+
+def firing_rate_app(ydata):
+    return integral_app(firing_rate(ydata))
