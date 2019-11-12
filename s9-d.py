cc53eb3195ec72f7c757f2aed22558037e17b061
yuetongc <yuetongyc@outlook.com>
2019-11-12 19:22:02 +0000

    added color bar to plots

--- s9-d.py
+++ s9-d.py
@@ -4,41 +4,46 @@ import matplotlib.pyplot as plt
 
 N_neuron = 100
 
-t_int = 0.1
-rest_time1 = 1000
-stim_time = 2000
-rest_time2 = 1000
-total_time = rest_time1 + stim_time + rest_time2
-N_point = int(total_time/t_int)
-
 attractor_model = model.MultiAttractorModel(N=N_neuron)
 V = attractor_model.init_voltage()
 noise = attractor_model.init_noise()
 
+t_int = attractor_model.t_int
+rest_time1 = 1000
+stim_time = 2000
+rest_time2 = 1500
+total_time = rest_time1 + stim_time + rest_time2
+N_point = int(total_time/t_int)
+
 V_matrix = np.zeros([N_neuron, N_point])
 
 c = 0
-for i in range(int(rest_time1 / t_int)):
+for i in range(int(rest_time1/t_int)):
     V_in = V
-    noise_in = attractor_model.ornstein_uhlenbeck_process(noise, t_int)
-    V = attractor_model.sim(V_in, noise, t_int, 0)
+    noise_in = attractor_model.ornstein_uhlenbeck_process(noise)
+    V = attractor_model.update(V_in, noise, 0)
     noise = noise_in
     V_matrix[:, c] = V
     c += 1
-for i in range(int(stim_time / t_int)):
+for i in range(int(stim_time/t_int)):
     V_in = V
-    noise_in = attractor_model.ornstein_uhlenbeck_process(noise, t_int)
-    V = attractor_model.sim(V_in, noise, t_int, 2)
+    noise_in = attractor_model.ornstein_uhlenbeck_process(noise)
+    V = attractor_model.update(V_in, noise, 2)
     noise = noise_in
     V_matrix[:, c] = V
     c += 1
-for i in range(int(rest_time2 / t_int)):
+for i in range(int(rest_time2/t_int)):
     V_in = V
-    noise_in = attractor_model.ornstein_uhlenbeck_process(noise, t_int)
-    V = attractor_model.sim(V_in, noise, t_int, 0)
+    noise_in = attractor_model.ornstein_uhlenbeck_process(noise)
+    V = attractor_model.update(V_in, noise, 0)
     noise = noise_in
     V_matrix[:, c] = V
     c += 1
 
-plt.imshow(V_matrix, interpolation='nearest', aspect='auto')
+fig, ax = plt.subplots()
+im = plt.imshow(V_matrix, interpolation='nearest', aspect='auto', extent=(0, total_time, -180, 180))
+ax.set_title('True Neuron Activity Plot')
+ax.set_xlabel('t')
+ax.set_ylabel('PO')
+fig.colorbar(im, ax=ax)
 plt.show()
