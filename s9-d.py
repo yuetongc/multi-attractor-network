0ebf28d2cd92b983681c3aab9bfaee22db23d9f0
yuetongc <yuetongyc@outlook.com>
2019-11-14 16:45:21 +0000

    change plot looking (remove box)

--- s9-d.py
+++ s9-d.py
@@ -9,13 +9,20 @@ V = attractor_model.init_voltage()
 noise = attractor_model.init_noise()
 
 t_int = attractor_model.t_int
-rest_time1 = 1000
-stim_time = 2000
-rest_time2 = 1500
+prep_time = 300
+rest_time1 = 500
+stim_time = 1000
+rest_time2 = 800
 total_time = rest_time1 + stim_time + rest_time2
 N_point = int(total_time/t_int)
 
 V_matrix = np.zeros([N_neuron, N_point])
+
+for i in range(int(prep_time/t_int)):
+    V_in = V
+    noise_in = attractor_model.ornstein_uhlenbeck_process(noise)
+    V = attractor_model.update(V_in, noise, 0)
+    noise = noise_in
 
 c = 0
 for i in range(int(rest_time1/t_int)):
@@ -42,8 +49,10 @@ for i in range(int(rest_time2/t_int)):
 
 fig, ax = plt.subplots()
 im = plt.imshow(V_matrix, interpolation='nearest', aspect='auto', extent=(0, total_time, -180, 180))
-ax.set_title('True Neuron Activity Plot')
-ax.set_xlabel('t')
-ax.set_ylabel('PO')
+ax.spines['top'].set_visible(False)
+ax.spines['bottom'].set_visible(False)
+ax.spines['right'].set_visible(False)
+ax.set_ylabel(r'PO / $\degree$')
+ax.set_xlabel('t / ms')
 fig.colorbar(im, ax=ax)
 plt.show()
