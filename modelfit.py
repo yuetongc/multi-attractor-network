7467ea14491e064e8d6bd3ec4a946be498430e20
yuetongc <yuetongyc@outlook.com>
2019-11-17 15:36:37 +0000

    optimize log(sigma^2) instead of sigma (for better optimization) and function to calculate r squared

--- modelfit.py
+++ modelfit.py
@@ -1,13 +1,18 @@
+import math
 import numpy as np
 
 
 def f_v(x, params):
-    a, mu, sig = params
-    return a * np.exp((np.cos(x - mu) - 1.) / np.power(sig, 2.))
+    a, mu, log_var = params
+    return a * np.exp((np.cos(x - mu) - 1.) / math.exp(log_var))
+
+
+def mse(y, ypred):
+    return np.sum(np.power(y - ypred, 2.))
 
 
 def mse_fv(params, xdata, ydata):
-    return np.sum(np.power(ydata - f_v(xdata, params), 2.))
+    return mse(ydata, f_v(xdata, params))
 
 
 def integral_app(ydata):
@@ -20,3 +25,7 @@ def firing_rate(V_in, gain=0.1, max_rate=100):
 
 def firing_rate_app(ydata):
     return integral_app(firing_rate(ydata))
+
+
+def r_squared(ydata, ypred):
+    return 1 - mse(ydata, ypred) / np.sum(np.power(ydata - np.mean(ydata), 2.))
