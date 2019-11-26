a2ba19b305a10a31a987f6134724ae15201dd41a
yuetongc <yuetongyc@outlook.com>
2019-11-26 13:44:25 +0000

    more trials, modified axis for plots

--- s10-b.py
+++ s10-b.py
@@ -9,16 +9,13 @@ N_neuron = 100
 attractor_model = model.MultiAttractorModel(N=N_neuron)
 
 t_int = attractor_model.t_int
-prep_time = 300
-rest_time1 = 500
-stim_time = 1000
-rest_time2 = 800
+prep_time, rest_time1, stim_time, rest_time2 = 300, 500, 1000, 800
 total_time = rest_time1 + stim_time + rest_time2
 t = np.arange(0, total_time, t_int)
 N_point = int(total_time/t_int)
 
 N_stim = 4
-N_trial = 30
+N_trial = 150
 
 var_matrix = np.zeros([N_stim, N_point])
 for k in range(N_stim):
@@ -61,13 +58,16 @@ for k in range(N_stim):
 
 fig, ax = plt.subplots()
 lines = []
-for k in range(N_stim):
-    lines += ax.plot(t, var_matrix[k], label='c={}'.format(str(k)))
-ax.legend(lines, [l.get_label() for l in lines])
-ax.axvspan(rest_time1, rest_time1 + stim_time, alpha=0.5, color='yellow')
+lines += ax.plot(t, var_matrix[0], label='c=0', color='dimgrey', linewidth=2)
+lines += ax.plot(t, var_matrix[1], label='c=1', color='darkgreen', linewidth=2)
+lines += ax.plot(t, var_matrix[2], label='c=2', color='forestgreen', linewidth=2)
+lines += ax.plot(t, var_matrix[3], label='c=3', color='lightgreen', linewidth=2)
+ax.legend(lines, [l.get_label() for l in lines], frameon=False)
+ax.axvspan(rest_time1, rest_time1 + stim_time, alpha=0.5, color='lightgrey')
 ax.spines['top'].set_visible(False)
 ax.spines['bottom'].set_visible(False)
 ax.spines['right'].set_visible(False)
-ax.set_ylabel(r'$\sqrt{ \langle{ Var_{trials}(V) \rangle }_{neurons}}$ / mV')
-ax.set_xlabel('t / ms')
+ax.set_ylabel(r'$\sqrt{ \langle{ Var_{trials}(V) \rangle }_{neurons}}$ / mV', fontsize=14)
+ax.set_xlabel('t / ms', fontsize=14)
+ax.tick_params(labelsize=12)
 plt.show()
