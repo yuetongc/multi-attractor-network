13724d6936467cf3db6c89e257adae976c66b477
yuetongc <yuetongyc@outlook.com>
2020-04-25 22:39:18 +0100

    tidy up plots for statistics of estimated parameters

--- var-est-params-stats.py
+++ var-est-params-stats.py
@@ -4,22 +4,35 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import modelfit
 
-
 import os
-os.chdir('/Users/yuetongyc/PycharmProjects/multi-attractor-model/data')
+"path for simulation data"
+os.chdir('/Users/yuetongyc/Desktop/Cambridge/IIB/Project/data')
 
 
-a_df = pd.read_csv('a_tau_20.csv', index_col=False)
-mu_df = pd.read_csv('mu_tau_20.csv', index_col=False)
-var_df = pd.read_csv('var_tau_20.csv', index_col=False)
-b_df = pd.read_csv('b_tau_20.csv', index_col=False)
+rc = {"axes.spines.left": True,
+      "axes.spines.right": False,
+      "axes.spines.bottom": False,
+      "axes.spines.top": False,
+      "lines.linewidth": 2,
+      "xtick.labelsize": 24,
+      "ytick.labelsize": 24,
+      'legend.fontsize': 24,
+      "axes.labelsize": 28,
+      }
+plt.rcParams.update(rc)
+
+
+a_df = pd.read_csv('a_tau_10_1.csv', index_col=False)
+mu_df = pd.read_csv('mu_tau_10_1.csv', index_col=False)
+var_df = pd.read_csv('var_tau_10_1.csv', index_col=False)
+b_df = pd.read_csv('b_tau_10_1.csv', index_col=False)
 
 a = np.transpose(a_df.values)
 mu = np.transpose(mu_df.values)
 var = np.transpose(var_df.values)
 b = np.transpose(b_df.values)
 
-v_var_df = pd.read_csv('v_var_tau_20.csv', index_col=False)
+v_var_df = pd.read_csv('v_var_tau_10_1.csv', index_col=False)
 v_var = v_var_df.values
 
 
@@ -27,6 +40,9 @@ a_mean = np.mean(a, axis=0)
 mu_mean = np.mean(mu, axis=0)
 var_mean = np.mean(var, axis=0)
 b_mean = np.mean(b, axis=0)
+
+circular_mu_mean = np.apply_along_axis(modelfit.circular_mean, 0, mu)
+circular_mu_var = np.apply_along_axis(modelfit.circular_variance, 0, mu)
 
 a_var = np.var(a, axis=0)
 mu_var = np.var(mu, axis=0)
@@ -37,6 +53,7 @@ v_variance = np.mean(v_var, axis=0)
 v_variability = np.sqrt(v_variance)
 
 
+N_trial = a.shape[0]
 N_neuron = 100
 N_point = 23000
 x = np.arange(-math.pi, math.pi, 2 * math.pi / N_neuron)
@@ -44,6 +61,7 @@ x = np.arange(-math.pi, math.pi, 2 * math.pi / N_neuron)
 prep_time, rest_time1, stim_time, rest_time2 = 300, 500, 1000, 800
 t0, t1, t2, t3 = prep_time, rest_time1, rest_time1+stim_time, rest_time1+stim_time+rest_time2
 t_tick = np.arange(0, t3, 0.1)
+
 
 x_mtx = np.reshape(np.repeat(x, N_point), (N_neuron, N_point))
 
@@ -118,128 +136,64 @@ exp_term = np.exp((np.cos(x_mtx - mu_mean_mtx) - 1) / var_mean_mtx)
 sin_term = np.sin(x_mtx - mu_mean_mtx)
 mu_grad_estimate = a_mean_mtx * exp_term * sin_term / var_mean_mtx
 
-
-font = {'size': 18}
-plt.rc('font', **font)
-
-rc = {"axes.spines.left": True,
-      "axes.spines.right": False,
-      "axes.spines.bottom": False,
-      "axes.spines.top": False}
-plt.rcParams.update(rc)
-
-
 parameter_pairs = [a_mu_term_avg, a_var_term_avg, a_b_term_avg, mu_var_term_avg, mu_b_term_avg, var_b_term_avg]
 pair_labels = [r'$Cov[\hat{a}, \hat{\mu}]}$', r'$Cov[\hat{a}, \hat{\sigma^{2}}]$', r'$Cov[\hat{a}, \hat{b}]$',
                r'$Cov[\hat{\mu}, \hat{\sigma^{2}}]$', r'$Cov[\hat{\mu}, \hat{b}]$', r'$Cov[\hat{\sigma^{2}}, \hat{b}]$']
 
-fig1, (ax1, ax2, ax3) = plt.subplots(3, 1, figsize=(12, 12))
-bottom, top = 0.1, 0.9
-left, right = 0.1, 0.85
-fig1.subplots_adjust(top=top, bottom=bottom, left=left, right=right, hspace=0.15, wspace=0.25)
-ax1.plot(t_tick, v_variance, color='dimgrey', label='true')
-ax1.plot(t_tick, v_variance_estimate, color='orange', label='estimate')
-ax1.set_ylabel(r'$\langle{Var_{trials}[V] \rangle }_{neurons}$')
+
+fig1, (ax1, ax2, ax3, ax4) = plt.subplots(4, 1, figsize=(12, 14))
+for n in range(N_trial):
+    ax1.plot(t_tick, a[n], color='black')
+    ax2.plot(t_tick, np.rad2deg(mu[n]), color='black')
+    ax3.plot(t_tick, var[n], color='black')
+    ax4.plot(t_tick, b[n], color='black')
+ax1.set_ylabel(r'$\hat{a}$ [mV]')
 ax1.axvspan(t1, t2, alpha=0.5, color='lightgrey')
-ax1.set_ylim(-1, 5)
-ax1.legend()
-ax2.plot(t_tick, a_term_avg, label=r'$Var[\hat{a}]}$', color='firebrick')
-ax2.plot(t_tick, mu_term_avg, label=r'$Var[\hat{\mu}]}$', color='mediumblue')
-ax2.plot(t_tick, var_term_avg, label=r'$Var[\hat{\sigma^{2}}]}$', color='darkgreen')
-ax2.plot(t_tick, b_term_avg, label=r'$Var[\hat{b}]}$', color='sandybrown')
-for i in range(len(parameter_pairs)):
-    "if not np.all(np.abs(parameter_pairs[i]) < 0.01):"
-    ax2.plot(t_tick, parameter_pairs[i], label=pair_labels[i])
-ax2.set_ylabel(r'$\alpha_{i}^{2}w_{i}$')
+ax2.set_ylabel(r'$\hat{\mu}\ [\degree]$')
 ax2.axvspan(t1, t2, alpha=0.5, color='lightgrey')
-ax2.set_ylim(-1, 5)
-ax2.legend(loc='upper right', ncol=2, fontsize=12)
-ax3.plot(t_tick, v_variance_estimate, color='orange', label='variance estimate')
-ax3.plot(t_tick, mu_term_avg, color='mediumblue', label=r'$\alpha_{\mu}^{2}w_{\mu}$')
-ax3.legend(loc='upper right')
-ax3.set_ylabel(r'$\langle{Var_{trials}[V] \rangle }$')
+ax2.set_yticks([-180, 0, 180])
+ax3.set_ylabel(r'$\hat{w}^{2}$')
 ax3.axvspan(t1, t2, alpha=0.5, color='lightgrey')
-ax3.set_ylim(-1, 5)
-ax3.set_xlabel('t / ms')
+ax4.set_ylabel(r'$\hat{b}$ [mV]')
+ax4.axvspan(t1, t2, alpha=0.5, color='lightgrey')
+ax4.set_xlabel('t [ms]')
 plt.tight_layout()
-fig1.savefig('Variance Estimation')
+fig1.savefig('parameter_stats')
 
 fig2, (ax1, ax2, ax3, ax4) = plt.subplots(4, 1, figsize=(12, 14))
-ax1.plot(t_tick, a_var, color='black')
-ax1.set_ylabel(r'$Var_{trail}[\hat{a}]}$')
+ax1.plot(t_tick, a_mean, color='black')
+ax1.set_ylabel(r'$\bar{a}$ [mV]')
 ax1.axvspan(t1, t2, alpha=0.5, color='lightgrey')
-ax2.plot(t_tick, mu_var, color='black')
-ax2.set_ylabel(r'$Var_{trail}[\hat{\mu}]}$')
+ax2.plot(t_tick, np.rad2deg(circular_mu_mean), color='black')
+ax2.set_ylabel(r'$\bar{\mu}\ [\degree]$')
+ax2.axvspan(t1, t2, alpha=0.5, color='lightgrey')
+ax2.set_yticks([-180, 0, 180])
+ax3.plot(t_tick, var_mean, color='black')
+ax3.set_ylabel(r'$\bar{w}^{2}$')
+ax3.axvspan(t1, t2, alpha=0.5, color='lightgrey')
+ax3.set_yticks([1.8, 1.9, 2])
+ax4.plot(t_tick, b_mean, color='black')
+ax4.set_ylabel(r'$\bar{b}$ [mV]')
+ax4.axvspan(t1, t2, alpha=0.5, color='lightgrey')
+ax4.set_xlabel('t [ms]')
+plt.tight_layout()
+fig2.savefig('parameter_mean')
+
+fig3, (ax1, ax2, ax3, ax4) = plt.subplots(4, 1, figsize=(12, 14))
+ax1.plot(t_tick, a_var, color='black')
+ax1.set_ylabel(r'$\sigma^{2}_{a}$')
+ax1.axvspan(t1, t2, alpha=0.5, color='lightgrey')
+ax2.plot(t_tick, circular_mu_var, color='black')
+ax2.set_ylabel(r'$\sigma^{2}_{\mu}$')
 ax2.axvspan(t1, t2, alpha=0.5, color='lightgrey')
 ax3.plot(t_tick, var_var, color='black')
-ax3.set_ylabel(r'$Var_{trail}[\hat{\sigma^{2}}]}$')
+ax3.set_ylabel(r'$\sigma^{2}_{w^{2}}$')
 ax3.axvspan(t1, t2, alpha=0.5, color='lightgrey')
 ax4.plot(t_tick, b_var, color='black')
-ax4.set_ylabel(r'$Var_{trail}[\hat{b}]}$')
+ax4.set_ylabel(r'$\sigma^{2}_{b}$')
 ax4.axvspan(t1, t2, alpha=0.5, color='lightgrey')
-ax4.set_xlabel('t / ms')
+ax4.set_xlabel('t [ms]')
 plt.tight_layout()
-fig2.savefig('Parameter Variance')
+fig3.savefig('parameter_var')
 
-fig3, (ax1, ax2, ax3, ax4, ax5, ax6) = plt.subplots(6, 1, figsize=(12, 18))
-ax1.plot(t_tick, a_mu_cov, color='black')
-ax1.set_ylabel(r'$Cov[\hat{a}, \hat{\mu}]}$')
-ax1.axvspan(t1, t2, alpha=0.5, color='lightgrey')
-ax2.plot(t_tick, a_var_cov, color='black')
-ax2.set_ylabel(r'$Cov[\hat{a}, \hat{\sigma^{2}}]$')
-ax2.axvspan(t1, t2, alpha=0.5, color='lightgrey')
-ax3.plot(t_tick, a_b_cov, color='black')
-ax3.set_ylabel(r'$Cov[\hat{a}, \hat{b}]$')
-ax3.axvspan(t1, t2, alpha=0.5, color='lightgrey')
-ax4.plot(t_tick, mu_var_cov, color='black')
-ax4.set_ylabel(r'$Cov[\hat{\mu}, \hat{\sigma^{2}}]$')
-ax4.axvspan(t1, t2, alpha=0.5, color='lightgrey')
-ax5.plot(t_tick, mu_b_cov, color='black')
-ax5.set_ylabel(r'$Cov[\hat{\mu}, \hat{b}]$')
-ax5.axvspan(t1, t2, alpha=0.5, color='lightgrey')
-ax6.plot(t_tick, var_b_cov, color='black')
-ax6.set_ylabel(r'$Cov[\hat{\sigma^{2}}, \hat{b}]$')
-ax6.axvspan(t1, t2, alpha=0.5, color='lightgrey')
-ax6.set_xlabel('t / ms')
-plt.tight_layout()
-fig3.savefig('Parameter Covariance')
 
-fig4, ax = plt.subplots(1, 1, figsize=(12, 4))
-ax.plot(t_tick, np.mean(np.square(a_grad), axis=0), color='firebrick', label='a')
-ax.plot(t_tick, np.mean(np.square(mu_grad), axis=0), color='mediumblue', label=r'$\mu$')
-ax.plot(t_tick, np.mean(np.square(var_grad), axis=0), color='darkgreen', label=r'$\sigma^{2}$')
-ax.plot(t_tick, np.mean(np.square(b_grad), axis=0), color='sandybrown', label='b')
-ax.axvspan(t1, t2, alpha=0.5, color='lightgrey')
-ax.set_ylabel(r'$\langle \alpha_{w}^{2} \rangle$')
-ax.set_xlabel('t / ms')
-ax.set_ylim(0, 3.5)
-ax.legend(loc='upper right', ncol=2)
-plt.tight_layout()
-fig4.savefig('Parameter Coefficient')
-
-fig5, (ax1, ax2, ax3, ax4, ax5) = plt.subplots(5, 1, figsize=(12, 20))
-bottom, top = 0.1, 0.9
-left, right = 0.1, 0.85
-fig5.subplots_adjust(top=top, bottom=bottom, left=left, right=right, hspace=0.15, wspace=0.25)
-ax1.plot(t_tick, np.mean(np.square(mu_grad_estimate), axis=0), color='mediumblue')
-ax1.set_ylabel(r'$\langle \alpha_{\mu}^{2} \rangle$')
-ax1.set_ylim(0, 3.5)
-ax1.axvspan(t1, t2, alpha=0.5, color='lightgrey')
-ax2.plot(t_tick, np.mean(np.square(a_mean_mtx), axis=0), color='black')
-ax2.set_ylabel(r'$\langle \bar{a}^{2} \rangle$')
-ax3.set_ylim(0.44, 0.47)
-ax2.axvspan(t1, t2, alpha=0.5, color='lightgrey')
-ax3.plot(t_tick, np.mean(np.square(exp_term), axis=0), color='black')
-ax3.set_ylabel(r'$\langle (\exp(\frac{\sin(\theta - \bar{\mu}) - 1}{\bar{\sigma}^{2}}))^{2} \rangle$')
-ax3.axvspan(t1, t2, alpha=0.5, color='lightgrey')
-ax4.plot(t_tick, np.mean(np.square(sin_term), axis=0), color='black')
-ax4.set_ylabel(r'$\langle \sin^{2}(\theta - \bar{\mu}) \rangle$')
-ax4.set_ylim(0.3, 0.7)
-ax4.axvspan(t1, t2, alpha=0.5, color='lightgrey')
-ax5.plot(t_tick, np.mean(np.square(1/var_mean_mtx), axis=0), color='black')
-ax5.set_ylabel(r'$\langle (\frac{1}{\bar{\sigma}^{2}})^{2} \rangle$')
-ax5.set_ylim(0.25, 0.3)
-ax5.axvspan(t1, t2, alpha=0.5, color='lightgrey')
-ax5.set_xlabel('t / ms')
-plt.tight_layout()
-fig5.savefig('Mu Parameter Coefficient')
